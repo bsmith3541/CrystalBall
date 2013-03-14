33f223ad410509bb26e64773b89b9b4718d2c9c0
Brandon Smith <brandon.smith@yale.edu>
2013-03-13 21:15:07 -0400

    Initial Commit

---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Brandon Smith on 3/13/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Brandon Smith on 3/13/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
